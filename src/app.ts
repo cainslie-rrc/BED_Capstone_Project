9dc125fd47610299233b970da6cfe895b2790937
Curtis Ainslie <cainslie@gmail.com>
2025-11-03 08:37:23 -0600

    Added app, server and httpconstants

---
+++ src/app.ts
@@ -0,0 +1,11 @@
+import express, { Express } from "express";
+
+const app: Express = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Hello World");
+});
+
+export default app;
